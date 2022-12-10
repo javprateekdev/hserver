878c5f89e4e23ea982d074ff2e13afb3eecf3516
javprateekdev <113234677+javprateekdev@users.noreply.github.com>
2022-12-10 11:33:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,37 @@ const cors = require('cors');
 
 
 const mysql = require("mysql");
-const con= mysql.createConnection({
-    host: "housethat.in",
-    user: "u406919292_tool",
-    password: "Abcd@321",
-    database: "u406919292_compare",
-});
+let connection=null;
+function handleDisconnect() {
+    connection = mysql.createConnection({
+      host: "housethat.in",
+      user: "u406919292_tool",
+      password: "Abcd@321",
+      database: "u406919292_compare",
+    }); // Recreate the connection, since
+    // the old one cannot be reused.
 
-con.connect((err)=>{
-    if(err)
-    {
-        console.warn("error in connection")
-    }
-});
+    connection.connect(function (err) { // The server is either down
+        if (err) { // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        } // to avoid a hot loop, and to allow our node script to
+    }); // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect(); // lost due to either server restart, or a
+        } else { // connnection idle timeout (the wait_timeout
+            throw err; // server variable configures this)
+        }
+    });
+}
+handleDisconnect();
+setInterval(function () {
+    connection.query('SELECT 1');
+}, 5000);
+
 app.use(express.json());
 app.use((err, req, res, next) => {
   console.log(err.stack);
@@ -30,7 +48,7 @@ app.use((err, req, res, next) => {
 app.use(cors());
 app.use(express.json());
 app.get("/data", (req, resp) => {
-  con.query("select * from project", (err, result) => {
+  connection.query("select * from project", (err, result) => {
     if (err) { resp.send("error in api") }
     else { resp.send(result) }
   })
